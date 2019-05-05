84761b85fb651a3d0811c20b1bffd8dd8d4e4cb7
Aneta Kobylarz <atena771@wp.pl>
2019-05-05 15:45:09 +0000

    added footer with contact form and button

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -1,39 +1,33 @@
-    queue()
+   queue()
     .defer(d3.json, "data/books.json")
     .await(makeGraphs);
     
-function makeGraphs(error, booksData) {
+    function makeGraphs(error, booksData) {
    var ndx = crossfilter(booksData);
    
    show_spend(ndx);
-   show_readed(ndx);
-   show_year(ndx);
-   show_type(ndx);
-   show_languages(ndx);
    show_author(ndx);
+   show_type(ndx);
+   show_readed(ndx);
+   show_language(ndx);
+   show_year(ndx);
    
-   
-   dc.renderAll();
-}
-
-function show_author(ndx){
-    var dim = ndx.dimension(dc.pluck("author"));
+    dc.renderAll();
+    
+    }
+    
+    
+    function show_year(ndx){
+    var dim = ndx.dimension(dc.pluck("year"));
     var group = dim.group();
     
-    dc.barChart("#author-balance")
-        .width(900)
-        .height(600)
-        .margins({top:10, right:70, bottom:30, left: 70})
+    dc.pieChart("#year-balance")
+        .height(500)
+        .radius(300)
+        .transitionDuration(500)
         .dimension(dim)
         .group(group)
-        .transitionDuration(500)
-        .x(d3.scale.ordinal())
-        .xUnits(dc.units.ordinal)
-        .elasticY(true)
-        .xAxisLabel("Author")
-        .yAxisLabel("Books")
-        .yAxis().ticks(10);
-    
+        .legend(dc.legend().gap(10))
 }
 
 function show_languages(ndx){
@@ -74,7 +68,6 @@ function show_readed(ndx){
         .yAxis().ticks(10)
 }
 
-
 function show_type(ndx){
     var dim = ndx.dimension(dc.pluck("type"));
     var group = dim.group();
@@ -86,6 +79,25 @@ function show_type(ndx){
         .dimension(dim)
         .group(group)
         .legend(dc.legend().gap(10))
+}
+
+function show_author(ndx){
+    var dim = ndx.dimension(dc.pluck("author"));
+    var group = dim.group();
+    
+    dc.barChart("#author-balance")
+        .width(700)
+        .height(500)
+        .margins({top:10, right:70, bottom:30, left: 70})
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("Author")
+        .yAxisLabel("Books")
+        .yAxis().ticks(10);
 }
 
 function show_spend(ndx){
@@ -109,29 +121,18 @@ function show_spend(ndx){
     var stackedChart = dc.barChart("#spend-balance");
     
     stackedChart
-        .width(700)
+        .width(500)
         .height(300)
         .dimension(place_dim)
         .group(spendByPlaceLanguagePolish, "Polish")
         .stack(spendByPlaceLanguageEnglish, "English")
         .x(d3.scale.ordinal())
-        .legend(dc.legend().x(420).y(0).itemHeight(15).gap(5));
+        .legend(dc.legend().x(380).y(0).itemHeight(10).gap(6));
     stackedChart.margins().right = 100;
     
 }
 
-function show_year(ndx){
-    var dim = ndx.dimension(dc.pluck("year"));
-    var group = dim.group();
-    
-    dc.pieChart("#year-balance")
-        .height(500)
-        .radius(300)
-        .transitionDuration(500)
-        .dimension(dim)
-        .group(group)
-        .legend(dc.legend().gap(10))
-}
+
 
         
         
